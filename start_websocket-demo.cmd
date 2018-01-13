21edcb5ae8816c220a4e950df8948bc65a57457f
Friedhold Matz <fmatz.com@gmail.com>
2018-01-13 13:41:03 +0100

    Add files via upload

---
+++ start_websocket-demo.cmd
@@ -0,0 +1,4 @@
+
+REM START frmsal WebSocket Demo
+
+java -jar C:\Oracle\Middleware\Oracle_Home\forms\java\frmsal.jar -url "http://localhost:9001/forms/frmservlet?config=standaloneapp&form=chk_websocket.fmx"
